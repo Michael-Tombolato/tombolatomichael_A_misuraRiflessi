a6dc1269d1260735abca65b3c707345d3d1fd5f3
Michael <michael.tombolato@barsanti.edu.it>
2019-03-02 11:53:13 +0100

    02/03/2019 - Proseguimento programma con aggiunta di buzzar, led, bottone

--- misuraRiflessi/misuraRiflessi.ino
+++ misuraRiflessi/misuraRiflessi.ino
@@ -1,25 +1,51 @@
 int led = 13;
 int buzzer = 11;
-#define BUTTON = 9;
-int cont = 0;
-int tempoLed = 0;
-int tempoSuono = 0;
+int button = 9;
+int stato;
+int tRandom;
+int tempoLed;
+int tempoSuono;
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(button, INPUT);
   pinMode(led, OUTPUT);
-  pinMode(BUTTON, INPUT);
+  pinMode(buzzer, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED, HIGH);
-  cont = digitalRead(BUTTON);
-  for (int i = 0; cont == HIGH; i++)
+  Serial.println("Premere un qualsiasi tasto per dar via al test");
+  while(Serial.available() == 0)
   {
-    tempoLed = tempoLed + 1;
-    if (cont == HIGH)
+    
+  }
+  stato = LOW;
+  tRandom = random(1000, 4000);
+  delay(tRandom);
+  digitalWrite(led, HIGH);
+  while (stato == LOW)
+  {
+    stato = digitalRead(button);
+    tempoLed = millis();
+    if (stato == HIGH)
     {
-      digitalWrite(LED, LOW);
-    } 
+      digitalWrite(led, LOW);
+      tempoLed = tempoLed-tRandom;//Errore
+      Serial.println(tempoLed);
+    }
+  }
+  stato = LOW;
+  tRandom = random(1000, 4000);
+  delay(tRandom);
+  tone(buzzer, HIGH);
+  while (stato == LOW)
+  {
+    stato = digitalRead(button);
+    tempoSuono = millis();
+    if (stato == HIGH)
+    {
+      digitalWrite(buzzer, LOW); 
+    }
   }
 }
